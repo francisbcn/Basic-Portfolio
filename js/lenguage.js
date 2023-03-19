7b63d5dd997d3c05a113e6fa9d003e579baa84ae
francisbcn <francis.pama@hotmail.com>
2023-03-19 01:19:18 +0100

    chnga

--- js/lenguage.js
+++ js/lenguage.js
@@ -1,25 +1,44 @@
-// Cuando se haga clic en el botón
- document.querySelector('#english-button').addEventListener('click', function() {
+// Declarar la variable global para el idioma
+let currentLanguage = 'es';
+
+// Función para cambiar el idioma a inglés
+function changeToEnglish() {
   // Hace una solicitud a la versión en inglés de tu sitio
   fetch('english-version.html')
     .then(response => response.text())
     .then(html => {
       // Reemplaza el contenido de la página actual con el contenido en inglés
       document.querySelector('body').innerHTML = html;
+      currentLanguage = 'en';
     });
-});
+}
 
-
- // Cuando se haga clic en el botón
- document.querySelector('#spanish-button').addEventListener('click', function() {
-  // Hace una solicitud a la versión en inglés de tu sitio
+// Función para cambiar el idioma a español
+function changeToSpanish() {
+  // Hace una solicitud a la versión en español de tu sitio
   fetch('index.html')
     .then(response => response.text())
     .then(html => {
       // Reemplaza el contenido de la página actual con el contenido en español
       document.querySelector('body').innerHTML = html;
+      currentLanguage = 'es';
     });
+}
+
+// Asigna el evento click al botón de inglés
+document.querySelector('#english-button').addEventListener('click', function() {
+  if (currentLanguage !== 'en') {
+    changeToEnglish();
+  }
 });
+
+// Asigna el evento click al botón de español
+document.querySelector('#spanish-button').addEventListener('click', function() {
+  if (currentLanguage !== 'es') {
+    changeToSpanish();
+  }
+});
+
 
 /* document.querySelector('#english-button').addEventListener('click', function() {
   window.location.href = "english-version.html";
