7d28183caaf0ef8967cd56676fa708c132a82ba3
francis Pama <120580110+francisbcn@users.noreply.github.com>
2023-04-20 17:03:39 +0200

    algo nuevo nor ecuerdo

--- js/lenguage.js
+++ js/lenguage.js
@@ -1,46 +1,4 @@
-// Declarar la variable global para el idioma
-let currentLanguage = 'es';
-
-// Función para cambiar el idioma a inglés
-function changeToEnglish() {
-  // Hace una solicitud a la versión en inglés de tu sitio
-  fetch('english-version.html')
-    .then(response => response.text())
-    .then(html => {
-      // Reemplaza el contenido de la página actual con el contenido en inglés
-      document.querySelector('body').innerHTML = html;
-      currentLanguage = 'en';
-    });
-}
-
-// Función para cambiar el idioma a español
-function changeToSpanish() {
-  // Hace una solicitud a la versión en español de tu sitio
-  fetch('index.html')
-    .then(response => response.text())
-    .then(html => {
-      // Reemplaza el contenido de la página actual con el contenido en español
-      document.querySelector('body').innerHTML = html;
-      currentLanguage = 'es';
-    });
-}
-
-// Asigna el evento click al botón de inglés
 document.querySelector('#english-button').addEventListener('click', function() {
-  if (currentLanguage !== 'en') {
-    changeToEnglish();
-  }
-});
-
-// Asigna el evento click al botón de español
-document.querySelector('#spanish-button').addEventListener('click', function() {
-  if (currentLanguage !== 'es') {
-    changeToSpanish();
-  }
-});
-
-
-/* document.querySelector('#english-button').addEventListener('click', function() {
   window.location.href = "english-version.html";
 });
 
@@ -48,11 +6,7 @@ document.querySelector('#spanish-button').addEventListener('click', function() {
   window.location.href = "index.html";
 });
 
- */
-
-
-
-/* let isEnglish = false;
+let isEnglish = false;
 
 document.querySelector('#english-button').addEventListener('click', function() {
   if (isEnglish) {
@@ -72,5 +26,5 @@ document.querySelector('#english-button').addEventListener('click', function() {
   }
   let flagIcon = document.querySelector('#english-button .fa-flag')
   flagIcon.classList.toggle('fa-flag-eng');
-});  */ 
+}); 
 
